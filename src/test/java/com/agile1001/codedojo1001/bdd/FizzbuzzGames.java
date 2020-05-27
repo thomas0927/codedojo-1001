ba15f3fc2040a28cc5f246050e952b9689fdb3f5
thomas0927@163.com <agile@2019BJ>
2020-05-28 00:33:54 +0800

    添加FizzBuzz

---
+++ src/test/java/com/agile1001/codedojo1001/bdd/FizzbuzzGames.java
@@ -0,0 +1,12 @@
+package com.agile1001.codedojo1001.bdd;
+
+import org.concordion.api.MultiValueResult;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class FizzbuzzGames extends BaseFixture {
+
+
+
+}
